de8d4e6faa465274cac030f64f26c9f090650f6e
Jose <jozylopez27@gmail.com>
2020-04-03 10:33:32 -0500

    first commit

---
+++ stacks.js
@@ -0,0 +1,14 @@
+class _Node {
+    constructor(value, next) {
+        this.value = value
+        this.next = next
+    }
+}
+
+class Stack {
+    constructor() {
+        this.top = null
+    }
+
+    push()
+}
