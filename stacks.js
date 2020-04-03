c7e794dc6b856523f2e05e937c9c2ee1805db7f7
Matthew Wagaman <averaqedev@gmail.com>
2020-04-03 09:28:58 -0700

    drill 4 complete

--- stacks.js
+++ stacks.js
@@ -1,63 +1,63 @@
 class _Node {
-    constructor(data, next) {
-        this.data = data
-        this.next = next
-    }
+  constructor(data, next) {
+    this.data = data;
+    this.next = next;
+  }
 }
 
 class Stack {
-    constructor() {
-        this.top = null
+  constructor() {
+    this.top = null;
+  }
+
+  push(data) {
+    if(this.top === null) {
+      this.top = new _Node(data, null);
+      return this.top;
     }
 
-    push(data) {
-        if(this.top === null) {
-            this.top = new _Node(data, null)
-            return this.top
-        }
+    const node = new _Node(data, this.top);
+    this.top = node;
+  }
 
-        const node = new _Node(data, this.top)
-        this.top = node
-    }
-
-    pop() {
-        const node = this.top
-        this.top = node.next
-        return node.data
-    }
+  pop() {
+    const node = this.top;
+    this.top = node.next;
+    return node.data;
+  }
 }
 
 const stackHelpers = {
-    peek(stack) {
-        return stack.top
-    },
-    isEmpty(stack) {
-        return stack.top === null
-    },
-    display(stack) {
-        if(stack.top === null) {
-            return `Stack is empty`
-        }
-        let start = stack
-
-        while(start.top !== null) {
-            console.log(start.pop())
-        }
+  peek(stack) {
+    return stack.top;
+  },
+  isEmpty(stack) {
+    return stack.top === null;
+  },
+  display(stack) {
+    if(stack.top === null) {
+      return 'Stack is empty';
     }
-}
+    let start = stack;
+
+    while(start.top !== null) {
+      console.log(start.pop());
+    }
+  }
+};
 
 
 function starTrek() {
-    let newStack = new Stack()
+  let newStack = new Stack();
 
-    newStack.push('kirk')
-    newStack.push('spock')
-    newStack.push('mccoy')
-    newStack.push('scotty')
-    newStack.pop()
-    newStack.pop()
+  newStack.push('kirk');
+  newStack.push('spock');
+  newStack.push('mccoy');
+  newStack.push('scotty');
+  newStack.pop();
+  newStack.pop();
 
-    stackHelpers.display(newStack)
+  stackHelpers.display(newStack);
 }
 
 // starTrek()
@@ -65,20 +65,20 @@ function starTrek() {
 
 // O(n)
 function is_palindrome(s) {
-    s = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
-    let newStack = new Stack()
+  s = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+  let newStack = new Stack();
 
-    for(let i = 0; i < s.length; i++) {
-        newStack.push(s[i])
-    }
+  for(let i = 0; i < s.length; i++) {
+    newStack.push(s[i]);
+  }
 
-    let str = ''
-    while(!stackHelpers.isEmpty(newStack)) {
-        let node = newStack.pop()
-        str += node
-    }
+  let str = '';
+  while(!stackHelpers.isEmpty(newStack)) {
+    let node = newStack.pop();
+    str += node;
+  }
 
-    return s === str
+  return s === str;
 }
 
 // True, true, true, false
@@ -87,6 +87,31 @@ function is_palindrome(s) {
 // console.log(is_palindrome("1001"));
 // console.log(is_palindrome("Tauhida"));
 
-function matching() {
-    // checking if the next character
+function matchingParentheses(expression) {
+  let holding = new Stack();
+  for(let i = 0; i < expression.length; i++) {
+    let c = expression[i];
+    if(c === '(') {
+      holding.push(c);
+    } else if(c === ')') {
+      if(stackHelpers.isEmpty(holding)) {
+        return 'you are missing a "("';
+      } else {
+        holding.pop();
+      }
+    }
+  }
+
+  if(!stackHelpers.isEmpty(holding)) {
+    return 'you are mising a ")"';
+  }
+  return true;
+}
+
+// console.log(matchingParentheses('(hello world)'));
+// console.log(matchingParentheses('hello world)'));
+// console.log(matchingParentheses('(hello world'));
+
+function sortStack() {
+    
 }
