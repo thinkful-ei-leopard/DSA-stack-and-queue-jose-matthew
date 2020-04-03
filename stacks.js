1e5f090fcb10fe45c2b24f76b9b5e3d22c518ba3
Jose <jozylopez27@gmail.com>
2020-04-03 11:17:51 -0500

    drill 4

--- stacks.js
+++ stacks.js
@@ -26,3 +26,67 @@ class Stack {
         return node.data
     }
 }
+
+const stackHelpers = {
+    peek(stack) {
+        return stack.top
+    },
+    isEmpty(stack) {
+        return stack.top === null
+    },
+    display(stack) {
+        if(stack.top === null) {
+            return `Stack is empty`
+        }
+        let start = stack
+
+        while(start.top !== null) {
+            console.log(start.pop())
+        }
+    }
+}
+
+
+function starTrek() {
+    let newStack = new Stack()
+
+    newStack.push('kirk')
+    newStack.push('spock')
+    newStack.push('mccoy')
+    newStack.push('scotty')
+    newStack.pop()
+    newStack.pop()
+
+    stackHelpers.display(newStack)
+}
+
+// starTrek()
+
+
+// O(n)
+function is_palindrome(s) {
+    s = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+    let newStack = new Stack()
+
+    for(let i = 0; i < s.length; i++) {
+        newStack.push(s[i])
+    }
+
+    let str = ''
+    while(!stackHelpers.isEmpty(newStack)) {
+        let node = newStack.pop()
+        str += node
+    }
+
+    return s === str
+}
+
+// True, true, true, false
+// console.log(is_palindrome("dad"));
+// console.log(is_palindrome("A man, a plan, a canal: Panama"));
+// console.log(is_palindrome("1001"));
+// console.log(is_palindrome("Tauhida"));
+
+function matching() {
+    // checking if the next character
+}
